23c03a2a2bf9826f46f2ce245b066709e828683b
Santeri Hiltunen <git@me.hilzu.moe>
2023-09-29 18:46:12 +0300

    Switch protobuf codegen to ts-proto for better types

--- packages/libpg-query-wasm/test/scan-test.mjs
+++ packages/libpg-query-wasm/test/scan-test.mjs
@@ -1,22 +1,32 @@
 import { describe, it } from "node:test";
 import { strict as assert } from "node:assert";
-import { scan, ScanResult } from "libpg-query-wasm";
+import { scan, pgQuery } from "libpg-query-wasm";
 
 describe("scan", () => {
   it("scans simple statement", () => {
-    const msg = scan("select 1");
-    assert.deepEqual(ScanResult.toObject(msg, { enums: String }), {
+    const result = scan("select 1");
+    assert.deepEqual(result, {
       version: 150001,
       tokens: [
-        { end: 6, token: "SELECT", keywordKind: "RESERVED_KEYWORD" },
-        { start: 7, end: 8, token: "ICONST" },
+        {
+          start: 0,
+          end: 6,
+          token: pgQuery.Token.SELECT,
+          keywordKind: pgQuery.KeywordKind.RESERVED_KEYWORD,
+        },
+        {
+          start: 7,
+          end: 8,
+          token: pgQuery.Token.ICONST,
+          keywordKind: pgQuery.KeywordKind.NO_KEYWORD,
+        },
       ],
     });
   });
 
   it("returns no tokens with empty query", () => {
-    const msg = scan("");
-    assert.deepEqual(msg.tokens, []);
+    const result = scan("");
+    assert.deepEqual(result.tokens, []);
   });
 
   it("throws on invalid argument", () => {
